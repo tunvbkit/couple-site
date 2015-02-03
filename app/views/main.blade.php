de4d75ae9784ce1f9c4a9343ab9d8da51f22b2cf
tunvbkit <tunvbkit@gmail.com>
2015-02-03 14:35:20 +0700

    update main

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -251,7 +251,19 @@
 var product_name = ''; if(_h1){product_name= _h1.textContent || _h1.innerText;}var ga = document.createElement('script'); ga.type = 'text/javascript';
 ga.src = '//live.vnpgroup.net/js/web_client_box.php?hash=82fcf1fcdbc86bb2ca315feba840b27a&data=eyJzc29faWQiOjEyNDA0MjQsImhhc2giOiJjNzRkNTY2YjQwYmY0YzZhODFmMmM0NTgzNjMyMTNkOCJ9&pname='+product_name;
 var s = document.getElementsByTagName('script');s[0].parentNode.insertBefore(ga, s[0]);})();
-</script>	
+</script>
+
+<!-- Google Analytic -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59288148-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 
 </body>
