ed8d7bb2556fc017518bab1f9ddb6f47be7aa273
tunvbkit <tunvbkit@gmail.com>
2015-02-12 13:41:24 +0700

    slide

--- app/views/business/my-slide.blade.php
+++ app/views/business/my-slide.blade.php
@@ -1,6 +1,9 @@
 
 	@foreach($slide as $slide)
-	  <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 one-slide">
+	  <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 one-slide one-slide{{$slide->id}}">
+	  		<span class="btn-delete">
+	            <i class="glyphicon glyphicon-remove-sign" onclick="delSlide({{$slide->id}})"></i>
+          	</span>
 	      <img  class="img-responsive img-thumbnail" src="{{Asset("../{$slide->bigpic}")}}">
 	  </div>
   	@endforeach()
